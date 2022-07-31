3fd22e46da4cea995de857ef94a85cd5345f21a3
Mahitha Valluru <mahitha.valluru@gmail.com>
2022-07-30 22:21:10 -0400

    Create readme.mk

---
+++ readme.mk
@@ -0,0 +1,12 @@
+# Welcome to my Senate Trading Activity Tracker Repository
+
+This repository will be to track senate trading activity.
+
+
+## Data
+
+Senator, former Senator, and candidate financial disclosure reports received since January 1, 2012 are available online, pursuant to the Ethics in Government Act (EIGA), as amended. Senator reports are available until six years after the individual ceases to be a Member. Candidate reports are available for one year after the individual is no longer a candidate.
+
+All financial disclosure reports filed under the EIGA, as amended, are available for viewing in the Senate Office of Public Records, 232 Senate Hart Building Washington, DC 20510, including reports for Senate and Commission employees. As required by the EIGA, as amended, employee reports are retained for a period of six years after the date of receipt.
+
+You can find the link to these files here: https://efdsearch.senate.gov/search/
